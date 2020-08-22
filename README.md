d6e82338fb337f897b54973f500e4c24d0ad71ca
mr.Shu <mr@shu.io>
2020-08-22 19:35:16 +0200

    init: First experiments
    * Add first experiments with the sample Soccer world that is provided in
      the base installation of Webots
    
    * Add a couple basic controllers -- `rcj_soccer_player.py` is a
      reimplementation of the C one into Python
    
    Signed-off-by: mr.Shu <mr@shu.io>

---
+++ README.md
@@ -0,0 +1,27 @@
+# RCJ Soccer Sim Webots Playground
+
+This repository serves as a playground for various experiments related to
+setting up a simulated RCJ Soccer environment in the Webots simulator.
+
+It currently features a sample robot as well as a supervisor. These are in an
+aplha stage and will very probably change in the future.
+
+## How do I try this out?
+
+1. Download [Webots](https://www.cyberbotics.com/])
+2. Clone this repository to your computer by running `git clone https://github.com/RoboCupJuniorTC/webots-soccer-sim-playground.git`
+3. Open the downloaded `soccer.wbt` world which you can find in the `worlds`
+   directory (via `File > Open World`)
+4. Run the simulation. Note that just one robot (`B2`) will use the Python
+   controler which is located at
+   `controllers/rcj_soccer_player/rcj_soccer_player.py`
+
+## Notes
+
+A specific `webots` world can be executed directly from the command line as
+follows:
+
+        webots --mode=run worlds/soccer.wbt
+
+Which allows for at least some automation. Further info can be found in the
+[docs](https://cyberbotics.com/doc/guide/starting-webots).
---
+++ controllers/rcj_soccer_player/rcj_soccer_player.py
@@ -0,0 +1,89 @@
+"""rcj_soccer_player controller."""
+from controller import Robot
+import struct
+import math
+
+TIME_STEP = 64
+ROBOT_NAMES = ["B1", "B2", "B3", "Y1", "Y2", "Y3"]
+N_ROBOTS = len(ROBOT_NAMES)
+
+
+def parse_supervisor_msg(packet: str) -> dict:
+    # X, Z and rotation for each robot
+    # plus X and Z for ball
+    struct_fmt = 'ddd' * 6 + 'dd'
+
+    unpacked = struct.unpack(struct_fmt, packet)
+
+    data = {}
+    for i, r in enumerate(ROBOT_NAMES):
+        data[r] = {
+            "x": unpacked[3 * i],
+            "y": unpacked[3 * i + 1],
+            "orientation": unpacked[3 * i + 2]
+        }
+    data["ball"] = {
+        "x": unpacked[3 * N_ROBOTS],
+        "y": unpacked[3 * N_ROBOTS + 1]
+    }
+    return data
+
+# create the Robot instance.
+robot = Robot()
+
+name = robot.getName()
+team = name[0]
+player_id = int(name[1])
+
+
+receiver = robot.getReceiver("receiver")
+receiver.enable(TIME_STEP)
+
+left_motor = robot.getMotor("left wheel motor")
+right_motor = robot.getMotor("right wheel motor")
+
+left_motor.setPosition(float('+inf'))
+right_motor.setPosition(float('+inf'))
+
+left_motor.setVelocity(0.0)
+right_motor.setVelocity(0.0)
+
+while robot.step(TIME_STEP) != -1:
+    if receiver.getQueueLength() > 0:
+        packet = receiver.getData()
+        receiver.nextPacket()
+
+        data = parse_supervisor_msg(packet)
+
+        # Get the position of our robot
+        robot_pos = data[name.upper()]
+        # Get the position of the ball
+        ball_pos = data['ball']
+
+        # Get the angle between the robot and the ball
+        angle = math.atan2(robot_pos['y'] - ball_pos['y'],
+                           robot_pos['x'] - ball_pos['x'])
+
+        # Find out the distance between the robot's orientation and the ball's
+        # angle
+        d = math.atan2(math.sin(angle - robot_pos['orientation']),
+                       math.cos(angle - robot_pos['orientation']))
+
+        degrees = math.degrees(d)
+
+        left_speed = -1
+        right_speed = 1
+
+        # If the robot has the ball right in front of it, go forward, otherwise
+        # rotate
+        if degrees >= 130 and degrees <= 160:
+            left_speed = 5
+            right_speed = 5
+        else:
+            multiplier = -1  # if degrees < 180 else 1
+            left_speed = multiplier * 2
+            right_speed = multiplier * -2
+
+        left_motor.setVelocity(left_speed)
+        right_motor.setVelocity(right_speed)
+        print('Positions:', robot_pos, ball_pos)
