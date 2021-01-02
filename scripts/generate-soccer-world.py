bc20035a02892dd04756711247dd4abe79988b55
mr.Shu <mr@shu.io>
2021-01-02 17:40:05 +0100

    add: Script for generating specific worlds
    * Add a script for generating specific Webots worlds based on some
      configuration. This allows us to change the constollers, images on
      robots, as well as colors of goals without changing the simulation
      logic.
    
    Signed-off-by: mr.Shu <mr@shu.io>

---
+++ scripts/generate-soccer-world.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+from string import Template
+from pathlib import Path
+import getopt
+import sys
+
+try:
+    options, _ = getopt.getopt(
+        sys.argv[1:],
+        '',
+        ['template=',
+         'blue_rgb=',
+         'yellow_rgb=',
+         'blue_png_url=',
+         'yellow_png_url=',
+         'controller_b1=',
+         'controller_b2=',
+         'controller_b3=',
+         'controller_y1=',
+         'controller_y2=',
+         'controller_y3=',
+         ]
+    )
+except getopt.GetoptError as err:
+    print('ERROR:', err)
+    sys.exit(1)
+
+
+template_path = None
+params = {}
+for option, value in options:
+    if option == '--template':
+        template_path = Path(value)
+    else:
+        clean_key = option.replace('--', '')
+        params[clean_key] = value
+
+if not template_path.exists():
+    print(f"ERROR: Provided path {template_path} does not exist")
+    sys.exit(1)
+
+s = Template(template_path.read_text())
+print(s.substitute(params), end='')
