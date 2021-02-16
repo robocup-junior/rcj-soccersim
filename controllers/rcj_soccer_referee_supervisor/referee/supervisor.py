9a4cf35fec11f652af6b68197db2a5a255fe4214
David Schwarz <rcj-soccer-sim-feb-21@davidschwarz.info>
2021-02-17 00:04:50 +0100

    Previous half time scores
    * closes #74

--- controllers/rcj_soccer_referee_supervisor/referee/supervisor.py
+++ controllers/rcj_soccer_referee_supervisor/referee/supervisor.py
@@ -1,6 +1,7 @@
 import math
 import struct
 import random
+import os
 
 from typing import List, Optional, Tuple
 
@@ -107,7 +108,10 @@ class RCJSoccerSupervisor(Supervisor):
         self._update_positions()
 
         self.ball_reset_timer = 0
-        self.score_blue = self.score_yellow = 0
+        score_blue = os.environ.get("TEAM_B_INITIAL_SCORE", "0")
+        self.score_blue = int(score_blue) if score_blue != "" else 0
+        score_yellow = os.environ.get("TEAM_Y_INITIAL_SCORE", "0")
+        self.score_yellow = int(score_yellow) if score_yellow != "" else 0
         # The team that ought to have the kickoff at the next restart
         self.team_to_kickoff = None
 
