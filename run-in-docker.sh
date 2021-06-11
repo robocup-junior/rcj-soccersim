8a2e820536939e89caba46007b1b143f306a117b
mr.Shu <mr@shu.io>
2021-06-11 08:11:36 +0200

    update: Quiet down `run-in-docker.sh`
    * Make sure that the installation procedures in `run-in-docker.sh` are
      quiet.
    
    Signed-off-by: mr.Shu <mr@shu.io>

--- run-in-docker.sh
+++ run-in-docker.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-apt update
-apt install -y python3-pip
-pip3 install numpy==1.20.2 scipy==1.6.3 --user
+# Note that this assumes a Debian distribution
+apt -qq update
+apt -qq install python3-pip
+pip3 -q install numpy==1.20.2 scipy==1.6.3 --user
 xvfb-run /usr/local/webots/webots --stdout --stderr --batch --mode=fast $1
