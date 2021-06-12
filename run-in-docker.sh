78b3b3303a079efb3a309cedf4e5c2a458668046
mr.Shu <mr@shu.io>
2021-06-12 11:23:59 +0200

    update: Silence `apt install` in `run-in-docker.sh`
    * Ensure the `apt install` command is actually silent in the output
      logs.
    
    Signed-off-by: mr.Shu <mr@shu.io>

--- run-in-docker.sh
+++ run-in-docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Note that this assumes a Debian distribution
 apt -qq update
-apt -qq install python3-pip -y
+apt -qq install python3-pip -y > /dev/null
 pip3 -q install numpy==1.20.2 scipy==1.6.3 --user
 xvfb-run /usr/local/webots/webots --stdout --stderr --batch --mode=fast $1
